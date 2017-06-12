202e9f42480b862187ff9c66880279e12f119647
charlesma4 <charlesma4@gmail.com>
2017-06-12 09:41:10 -0400

    TODO: make css work

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -2,31 +2,47 @@ package com.teamtreehouse.courses;
 
 import com.teamtreehouse.courses.model.CourseIdea;
 import com.teamtreehouse.courses.model.CourseIdeaDAO;
+import com.teamtreehouse.courses.model.NotFoundException;
 import com.teamtreehouse.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 /**
  * Created by charl on 6/8/2017.
  */
 public class Main {
 
+    private static final String FLASH_MESSAGE_KEY = "flash_message";
+
     public static void main(String[] args) {
-
         staticFileLocation("/public =");
-
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        before((req, res) ->  {
+            if (req.cookie("username")!=null) {
+                req.attribute("username", req.cookie("username"));
+            }
+        });
+
+        before("/ideas", (req, res) -> {
+            if (req.attribute("username")==null) {
+                setFlashMessage(req, "Whoops! You don't have a username! Please enter one :)");
+
+                res.redirect("/");
+                halt();
+            }
+        });
 
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
+            model.put("username", req.attribute("username"));
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -35,24 +51,74 @@ public class Main {
             String username = req.queryParams("username");
             res.cookie("username", username);
             model.put("username", username);
-            return new ModelAndView(model, "sign-in.hbs");
-        }, new HandlebarsTemplateEngine());
+            res.redirect("/");
+            return null;
+        });
 
         get("/ideas", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("ideas", dao.findAll());
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/ideas", (req, res) -> {
             String title = req.queryParams("title");
-            //TO-DO: This username is tied to cookie implementation
             CourseIdea courseIdea = new CourseIdea(title,
-                    req.cookie("username"));
+                    req.attribute("username"));
             dao.add(courseIdea);
             res.redirect("/ideas");
             return null;
         });
+
+        get("/ideas/:slug", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("idea", dao.findBySlug(req.params("slug")));
+            return new ModelAndView(model, "voterUsers.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas/:slug/vote", (req, res) -> {
+            CourseIdea idea = dao.findBySlug(req.params("slug"));
+            boolean added = idea.addVoter(req.attribute("username"));
+            if(added) {
+                setFlashMessage(req, "Thanks for your vote!");
+            }
+            else {
+                setFlashMessage(req, "You already voted.");
+            }
+            res.redirect("/ideas");
+            return null;
+        });
+
+        exception(NotFoundException.class, (exc, req, res) ->  {
+            res.status(404);
+            HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+            String html = engine.render(
+                    new ModelAndView(null, "not-found.hbs"));
+            res.body(html);
+        });
+    }
+
+    private static void setFlashMessage(Request req, String msg) {
+        req.session().attribute(FLASH_MESSAGE_KEY, msg);
+    }
+
+    private static String getFlashMessage(Request req) {
+        if(req.session(false)==null) {
+            return null;
+        }
+        if(!req.session().attributes().contains(FLASH_MESSAGE_KEY)) {
+            return null;
+        }
+        return (String) req.session().attribute(FLASH_MESSAGE_KEY);
+    }
+
+    private static String captureFlashMessage(Request req) {
+        String message = getFlashMessage(req);
+        if(message != null) {
+            req.session().removeAttribute(FLASH_MESSAGE_KEY);
+        }
+        return message;
     }
 
 }
---
+++ src/main/java/com/teamtreehouse/courses/model/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse.courses.model;
+
+/**
+ * Created by charl on 6/9/2017.
+ */
+public class NotFoundException extends RuntimeException {
+}
